d47e44dfafe25a641a84881fde1c6ffaff3c6c4b
Olivier Levasseur <olivier.levasseur@cleveron.com>
2021-08-18 20:04:40 +0300

    Step 2

--- blockchain.py
+++ blockchain.py
@@ -5,3 +5,12 @@ Created on Sat Jun 12 11:58:57 2021
 @author: levas
 """
 
+import datetime
+import hashlib
+import json
+from flask import Flask, jsonify
+
+class Blockchain:
+    def __init__(self):
+        self.chain = []
+        self.create__block(proof = 1, previous_hash = '0')
