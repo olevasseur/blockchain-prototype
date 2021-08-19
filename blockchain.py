4544ccf34eb038570b7f5a5b57d8f7b9157deec3
Olivier Levasseur <levasseuro.ol@gmail.com>
2021-08-19 21:35:45 +0300

    Step 4

--- blockchain.py
+++ blockchain.py
@@ -14,3 +14,11 @@ class Blockchain:
     def __init__(self):
         self.chain = []
         self.create__block(proof = 1, previous_hash = '0')
+        
+    def create_block(self, proof, previous_hash):
+         block = {'index': len(self.chain) + 1, 
+                  'timestamp': str(datetime.datetime.now()),
+                  'proof': proof,
+                  'previous_hash': previous_hash}
+         self.chain.append(block)
+         return block
