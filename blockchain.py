c7558724c0a495b04a6813ad42b2816d06a9a56d
Olivier Levasseur <levasseuro.ol@gmail.com>
2021-08-21 19:52:26 +0300

    Step 8

--- blockchain.py
+++ blockchain.py
@@ -22,3 +22,37 @@ class Blockchain:
                   'previous_hash': previous_hash}
          self.chain.append(block)
          return block
+     
+    def get_previous_block(self):
+        return self.chain[-1]
+    
+    def proof_of_word(self, previous_proof):
+        new_proof = 1
+        check_proof = False
+        while not check_proof:
+            hash_operation = hashlib.sha256(str(new_proof **2 - previous_proof **2).encode()).hexdigest()
+            if hash_operation[:4] == '0000':
+                check_proof = True
+            else:
+                new_proof += 1
+        return new_proof
+    
+     def hash(self, block):
+         encoded_block = json.dumps(block, sort_keys = True).encode()
+         return hashlib.sha256(encoded_block)
+     
+     def is_chain_valid(self):
+         block_index = 1
+         previous_block = self.chain[0]
+         while block_index < len(self.chain):
+             block = chain[block_index]
+             if block['previous_hash'] != self.hash(previous_block):
+                 return False
+             previous_proof = previous_block['proof']
+             proof = block['proof']
+             hash_operation = hashlib.sha256(str(proof **2 - previous_proof **2).encode()).hexdigest()
+             if hash_operation[:4] != '0000':
+                 retur False
+             previous_block = block
+             block_index += 1
+         return True
